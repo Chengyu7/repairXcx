94a77603c85aa17155421d2367d1c4e82134dc4a
Chengyu7 <13472513493@163.com>
2022-04-26 20:52:08 +0800

    Default Changelist

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from controller.knowledge.knowledge import knowledge
 from exts import db
 
 app = Flask(__name__)
-app.config['REDIS_HOST'] = "127.0.0.1"  # redis数据库地址
+app.config['REDIS_HOST'] = "127.0.0.1"  # redis数据库地址，11
 app.config['REDIS_PORT'] = 6379  # redis 端口号
 app.config['REDIS_DB'] = 0  # 数据库名
 app.config['REDIS_EXPIRE'] = 60  # redis 过期时间60秒
